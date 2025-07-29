bc177f0668ad7aff48f054d2735674e98bc7a857
jaymaina-8 <jaymaina19@gmail.com>
2025-07-29 09:51:35 +0300

    commit

--- hospital/settings.py
+++ hospital/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'patients',        # database name
+        'USER': 'postgres',      # db user
+        'PASSWORD': 'hp101',  # password
+        'HOST': 'localhost',          # or your server IP
+        'PORT': '5432',               # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
